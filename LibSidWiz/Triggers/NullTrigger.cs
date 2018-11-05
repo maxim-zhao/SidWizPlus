818d81b5e0cf0adef64f6dfa62bf8360ea4fff86
Maxim <maxim@smspower.org>
2018-11-05 10:26:43 +0000

    Null trigger algorithm

---
+++ LibSidWiz/Triggers/NullTrigger.cs
@@ -0,0 +1,14 @@
+﻿namespace LibSidWiz.Triggers
+{
+    /// <summary>
+    /// Null algorithm just returns the first sample it's given
+    /// </summary>
+    // ReSharper disable once UnusedMember.Global
+    internal class NullTrigger: ITriggerAlgorithm
+    {
+        public int GetTriggerPoint(Channel channel, int startIndex, int endIndex)
+        {
+            return startIndex;
+        }
+    }
+}
