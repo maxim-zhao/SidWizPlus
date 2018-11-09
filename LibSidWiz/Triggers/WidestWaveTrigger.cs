80474dfaabc7658f32be0f94bf9fd7c9e56619a1
Maxim <maxim@smspower.org>
2018-11-09 00:36:00 +0000

    Tweaks to WidestWaveTrigger - don't double the range (there's a control for that), comment was out of date

--- LibSidWiz/Triggers/WidestWaveTrigger.cs
+++ LibSidWiz/Triggers/WidestWaveTrigger.cs
@@ -4,17 +4,15 @@
     /// Finds the widest positive+negative wave in the range
     /// This can get confused by volume changes on SN76489 noise, which it will perceive as a wide wave
     /// </summary>
-    class WidestWaveTrigger : ITriggerAlgorithm
+    // ReSharper disable once UnusedMember.Global
+    internal class WidestWaveTrigger : ITriggerAlgorithm
     {
         public int GetTriggerPoint(Channel channel, int startIndex, int endIndex)
         {
-            int bestOffset = (startIndex + endIndex) / 2; // Default to centre if no positive waves found
+            int bestOffset = (startIndex + endIndex) / 2; // Default to centre if no waves found
             int lastCrossingPoint = endIndex;
             float previousSample = channel.GetSample(startIndex);
             int bestLength = 0;
-
-            // We want to look beyond the end of the range to avoid losing track when the wave is low frequency
-            endIndex += endIndex - startIndex;
 
             // For each sample...
             for (int i = startIndex + 1; i < endIndex; ++i)
