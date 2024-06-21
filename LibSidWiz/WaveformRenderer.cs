817eba5f33770f9ba5e58b26223d7a13ec3eeccf
Maxim <maxim@smspower.org>
2024-06-21 23:03:48 +0100

    Initial multithreaded effort

--- LibSidWiz/WaveformRenderer.cs
+++ LibSidWiz/WaveformRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -6,6 +7,8 @@ using System.Drawing.Imaging;
 using System.Drawing.Text;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 using LibSidWiz.Outputs;
 
 namespace LibSidWiz
@@ -33,45 +36,24 @@ namespace LibSidWiz
 
         public void Render(IList<IGraphicsOutput> outputs)
         {
-            // This is the raw data buffer we use to store the generated image.
-            // We need it in this form, so we can pass it to FFMPEG.
-            var rawData = new byte[Width * Height * 4];
-            // We also need to "pin" it so the bitmap can be based on it.
-            GCHandle pinnedArray = GCHandle.Alloc(rawData, GCHandleType.Pinned);
-            try
-            {
-                using var bm = new Bitmap(Width, Height, Width * 4, PixelFormat.Format32bppPArgb, pinnedArray.AddrOfPinnedObject());
-                int numFrames = (int)(_channels.Max(c => c.SampleCount) * FramesPerSecond / SamplingRate);
-                var length = TimeSpan.FromSeconds((double)numFrames / FramesPerSecond);
+            int numFrames = (int)(_channels.Max(c => c.SampleCount) * FramesPerSecond / SamplingRate);
+            var length = TimeSpan.FromSeconds((double)numFrames / FramesPerSecond);
 
-                int frameIndex = 0;
-                Render(bm, rawData, () =>
-                    {
-                        double fractionComplete = (double) ++frameIndex / numFrames;
-                        foreach (var output in outputs)
-                        {
-                            // ReSharper disable once AccessToDisposedClosure
-                            // bm is disposed after Render() returns, but it never invokes this 
-                            output.Write(bm, rawData, fractionComplete, length);
-                        }
-                    },
-                    0, numFrames);
-            }
-            finally
+            int frameIndex = 0;
+            Render(0, numFrames, (bm, rawData) =>
             {
-                pinnedArray.Free();
-            }
+                double fractionComplete = (double) ++frameIndex / numFrames;
+                foreach (var output in outputs)
+                {
+                    output.Write(bm, rawData, fractionComplete, length);
+                }
+            });
         }
 
         /// <summary>
         /// Renders a range of frames into the given destination image, calling back the handler for each one
         /// </summary>
-        /// <param name="destination"></param>
-        /// <param name="onFrame"></param>
-        /// <param name="startFrame"></param>
-        /// <param name="endFrame"></param>
-        /// <param name="imageBuffer"></param>
-        private void Render(Image destination, byte[] imageBuffer, Action onFrame, int startFrame, int endFrame)
+        private void Render(int startFrame, int endFrame, Action<Bitmap, byte[]> onFrame)
         {
             // Default rendering bounds if not set
             var renderingBounds = RenderingBounds;
@@ -101,111 +83,208 @@ namespace LibSidWiz
             GCHandle pinnedArray = GCHandle.Alloc(templateData, GCHandleType.Pinned);
             try
             {
-                using var templateImage = new Bitmap(Width, Height, Width * 4, PixelFormat.Format32bppPArgb,
-                    pinnedArray.AddrOfPinnedObject());
+                using var templateImage = new Bitmap(Width, Height, Width * 4, PixelFormat.Format32bppPArgb, pinnedArray.AddrOfPinnedObject());
                 GenerateTemplate(templateImage);
-                using var g = Graphics.FromImage(destination);
-                // Prepare the pens and brushes we will use
-                var pens = _channels.Select(c => c.LineColor == Color.Transparent || c.LineWidth <= 0
-                    ? null
-                    : new Pen(c.LineColor, c.LineWidth)
-                    {
-                        MiterLimit = c.LineWidth,
-                        LineJoin = LineJoin.Bevel
-                    }).ToList();
-                var brushes = _channels.Select(c => c.FillColor == Color.Transparent
-                    ? null
-                    : new SolidBrush(c.FillColor)).ToList();
 
-                // Prepare buffers to hold the line coordinates
-                var buffers = _channels.Select(channel => new PointF[channel.ViewWidthInSamples]).ToList();
-                var path = new GraphicsPath();
-
-                var frameSamples = SamplingRate / FramesPerSecond;
-
+                // We want to do a few things in parallel:
+                // 1. For each frame, for each channel, get the previous frame trigger point
+                // 2. Find the next trigger point
+                // 3. When a frame has all the channels' trigger points, draw it
+                // 4. Call the callback in strict frame order
+                // #3 is the slowest part, but we'd like to somewhat parallelize parts 1-2 as well.
+                // So we do that in a parallel for, in a task, emitting into a queue...
+                var queue = new BlockingCollection<FrameInfo>(1000);
                 // Initialise the "previous trigger points"
+                var frameSamples = SamplingRate / FramesPerSecond;
                 var triggerPoints = new int[_channels.Count];
                 for (int channelIndex = 0; channelIndex < _channels.Count; ++channelIndex)
                 {
                     triggerPoints[channelIndex] =
-                        (int) ((long) startFrame * SamplingRate / FramesPerSecond) - frameSamples;
+                        (int)((long)startFrame * SamplingRate / FramesPerSecond) - frameSamples;
                 }
 
-                // Formatting for error/progress messages
-                var stringFormat = new StringFormat
-                    {LineAlignment = StringAlignment.Center, Alignment = StringAlignment.Center};
-
-                for (int frameIndex = startFrame; frameIndex < endFrame; ++frameIndex)
+                Task.Factory.StartNew(() =>
                 {
-                    // Compute the start of the sample window
-                    int frameIndexSamples = (int) ((long) frameIndex * SamplingRate / FramesPerSecond);
-
-                    // Copy from the template
-                    if (imageBuffer == null)
+                    for (var frameIndex = startFrame; frameIndex <= endFrame; ++frameIndex)
                     {
-                        g.DrawImageUnscaled(templateImage, 0, 0);
-                    }
-                    else
-                    {
-                        Buffer.BlockCopy(templateData, 0, imageBuffer, 0, templateData.Length);
-                    }
+                        // Compute the start of the sample window
+                        int frameIndexSamples = (int)((long)frameIndex * SamplingRate / FramesPerSecond);
+                        var info = new FrameInfo
+                        {
+                            FrameIndex = frameIndex,
+                            ChannelTriggerPoints = Enumerable.Repeat(frameIndexSamples, _channels.Count).ToList()
+                        };
+                        // For each channel...
+                        for (int channelIndex = 0; channelIndex < _channels.Count; ++channelIndex)
+                        {
+                            var channel = _channels[channelIndex];
+                            if (channel.IsEmpty ||
+                                !string.IsNullOrEmpty(channel.ErrorMessage) ||
+                                channel.Loading ||
+                                channel.IsSilent)
+                            {
+                                // No trigger to find
+                                continue;
+                            }
 
-                    // For each channel...
-                    for (int channelIndex = 0; channelIndex < _channels.Count; ++channelIndex)
-                    {
-                        var channel = _channels[channelIndex];
-                        if (channel.IsEmpty)
-                        {
-                            continue;
-                        }
-
-                        if (!string.IsNullOrEmpty(channel.ErrorMessage))
-                        {
-                            g.DrawString(channel.ErrorMessage, SystemFonts.DefaultFont, Brushes.Red,
-                                channel.Bounds,
-                                stringFormat);
-                        }
-                        else if (channel.Loading)
-                        {
-                            g.DrawString("Loading data...", SystemFonts.DefaultFont, Brushes.Green,
-                                channel.Bounds,
-                                stringFormat);
-                        }
-                        else if (channel.IsSilent && !channel.RenderIfSilent)
-                        {
-                            g.DrawString("This channel is silent", SystemFonts.DefaultFont, Brushes.Yellow,
-                                channel.Bounds, stringFormat);
-                        }
-                        else
-                        {
                             // Compute the "trigger point". This will be the centre of our rendering.
                             var triggerPoint = channel.GetTriggerPoint(frameIndexSamples, frameSamples,
                                 triggerPoints[channelIndex]);
+                            info.ChannelTriggerPoints[channelIndex] = triggerPoint;
                             triggerPoints[channelIndex] = triggerPoint;
-
-                            RenderWave(g, channel, triggerPoint, pens[channelIndex], brushes[channelIndex],
-                                buffers[channelIndex], path, channel.FillBase);
                         }
+
+                        // Add to the queue (may block)
+                        queue.Add(info);
                     }
 
-                    // Emit
-                    onFrame();
+                    // Signal that this phase is done
+                    queue.CompleteAdding();
+                });
+
+                // Then we want to make a bunch of objects to consume from the queue, as it is filled up, in parallel.
+                // These are tricky because they will want to emit bitmaps, which need to be passed to the renderers in order.
+                // To do that, a simple FIFO is not enough; we want it to also be ordered by frame index and only consume in order.
+                var renderedFrames = new ConcurrentDictionary<int, FrameInfo>();
+                var frameReadySignal = new AutoResetEvent(false);
+                // Then we kick off some parallel threads to do the rendering, consuming from queue and emitting to renderedFrames indexed by frame index
+                // Formatting for error/progress messages
+                var stringFormat = new StringFormat
+                    { LineAlignment = StringAlignment.Center, Alignment = StringAlignment.Center };
+
+                foreach (var _ in Enumerable.Range(0, 1))
+                {
+                    Task.Factory.StartNew(() =>
+                    {
+                        // We make a thread-local image to render into
+                        // This is the raw data buffer we use to store the generated image.
+                        // We need it in this form, so we can pass it to FFMPEG.
+                        var rawData = new byte[Width * Height * 4];
+                        // We also need to "pin" it so the bitmap can be based on it.
+                        var innerPinnedArray = GCHandle.Alloc(rawData, GCHandleType.Pinned);
+
+                        // Prepare the pens and brushes we will use
+                        var pens = _channels.Select(c => c.LineColor == Color.Transparent || c.LineWidth <= 0
+                            ? null
+                            : new Pen(c.LineColor, c.LineWidth)
+                            {
+                                MiterLimit = c.LineWidth,
+                                LineJoin = LineJoin.Bevel
+                            }).ToList();
+                        var brushes = _channels.Select(c => c.FillColor == Color.Transparent
+                            ? null
+                            : new SolidBrush(c.FillColor)).ToList();
+
+                        try
+                        {
+                            using var bm = new Bitmap(Width, Height, Width * 4, PixelFormat.Format32bppPArgb,
+                                innerPinnedArray.AddrOfPinnedObject());
+                            using var g = Graphics.FromImage(bm);
+
+                            // Prepare buffers to hold the line coordinates
+                            var buffers = _channels.Select(channel => new PointF[channel.ViewWidthInSamples]).ToList();
+                            var path = new GraphicsPath();
+
+                            while (!queue.IsCompleted)
+                            {
+                                // Get a frame to work on. This may block.
+                                var frame = queue.Take();
+
+                                // Copy from the template
+                                Buffer.BlockCopy(templateData, 0, rawData, 0, templateData.Length);
+
+                                // For each channel...
+                                for (var channelIndex = 0; channelIndex < _channels.Count; ++channelIndex)
+                                {
+                                    var channel = _channels[channelIndex];
+                                    if (channel.IsEmpty) continue;
+
+                                    if (!string.IsNullOrEmpty(channel.ErrorMessage))
+                                        // ReSharper disable once AccessToDisposedClosure
+                                        g.DrawString(
+                                            channel.ErrorMessage,
+                                            SystemFonts.DefaultFont,
+                                            Brushes.Red,
+                                            channel.Bounds,
+                                            stringFormat);
+                                    else if (channel.Loading)
+                                        // ReSharper disable once AccessToDisposedClosure
+                                        g.DrawString(
+                                            "Loading data...",
+                                            SystemFonts.DefaultFont,
+                                            Brushes.Green,
+                                            channel.Bounds,
+                                            stringFormat);
+                                    else if (channel.IsSilent && !channel.RenderIfSilent)
+                                        // ReSharper disable once AccessToDisposedClosure
+                                        g.DrawString(
+                                            "This channel is silent",
+                                            SystemFonts.DefaultFont,
+                                            Brushes.Yellow,
+                                            channel.Bounds,
+                                            stringFormat);
+                                    else
+                                        // ReSharper disable once AccessToDisposedClosure
+                                        RenderWave(g, channel, frame.ChannelTriggerPoints[channelIndex],
+                                            pens[channelIndex], brushes[channelIndex],
+                                            buffers[channelIndex], path, channel.FillBase);
+                                }
+
+                                // We "lend" the data to the frame info temporarily
+                                frame.Bitmap = bm;
+                                frame.RawData = rawData;
+                                renderedFrames.TryAdd(frame.FrameIndex, frame);
+                                // Signal the consuming thread
+                                frameReadySignal.Set();
+                                // Wait for it to finish consuming
+                                frame.BitmapConsumed.WaitOne();
+                            }
+                        }
+                        finally
+                        {
+                            innerPinnedArray.Free();
+                            foreach (var pen in pens)
+                            {
+                                pen?.Dispose();
+                            }
+
+                            foreach (var brush in brushes)
+                            {
+                                brush?.Dispose();
+                            }
+                        }
+                    });
                 }
 
-                foreach (var pen in pens)
+                // Finally, we consume the queue
+                for (var frameIndex = startFrame; frameIndex < endFrame; frameIndex++)
                 {
-                    pen?.Dispose();
-                }
+                    FrameInfo frame;
+                    while (!renderedFrames.TryGetValue(frameIndex, out frame))
+                    {
+                        // Wait for the dictionary to have this index
+                        frameReadySignal.WaitOne();
+                    }
 
-                foreach (var brush in brushes)
-                {
-                    brush?.Dispose();
+                    // Emit the frame
+                    onFrame(frame.Bitmap, frame.RawData);
+                    frame.BitmapConsumed.Set();
+                    renderedFrames.TryRemove(frameIndex, out _);
                 }
             }
             finally
             {
                 pinnedArray.Free();
             }
+        }
+
+        private class FrameInfo
+        {
+            public int FrameIndex { get; set; }
+            public List<int> ChannelTriggerPoints { get; set; }
+            public Bitmap Bitmap { get; set; }
+            public byte[] RawData { get; set; }
+            public AutoResetEvent BitmapConsumed { get; } = new(false);
         }
 
         private void GenerateTemplate(Image template)
@@ -387,7 +466,7 @@ namespace LibSidWiz
             // Then draw them all in one go...
             if (pen != null)
             {
-                // TODO: this is expensive
+                // This is the slowest part...
                 g.DrawLines(pen, points);
             }
 
@@ -412,8 +491,11 @@ namespace LibSidWiz
             var frameIndex = _channels.Count > 0
                 ? (int) (position * _channels.Max(c => c.SampleCount) * FramesPerSecond / SamplingRate)
                 : 0;
-            var bitmap = new Bitmap(Width, Height, PixelFormat.Format32bppPArgb);
-            Render(bitmap, null, () => { }, frameIndex, frameIndex + 1);
+            Bitmap bitmap = null;
+            Render(frameIndex, frameIndex + 1, (bm, _) =>
+            {
+                bitmap = (Bitmap)bm.Clone();
+            });
             return bitmap;
         }
     }
